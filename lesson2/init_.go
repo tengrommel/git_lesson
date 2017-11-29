4c2027cb3674846a08604f7cb02a400779e5da57
teng <wwwsousoucom@126.com>
2017-11-29 21:17:29 +0800

    bool初始值

--- lesson2/init_.go
+++ lesson2/init_.go
@@ -4,6 +4,7 @@ import "fmt"
 
 /*
 初值
+bool值的默认值为false
  */
 
 func main() {
@@ -12,9 +13,11 @@ func main() {
 		y float32
 		z string
 		p *int
+		boolValue bool
 	)
 	fmt.Printf("%v\n", x)
 	fmt.Printf("%v\n", y)
 	fmt.Printf("%v\n", z)
 	fmt.Printf("%v\n", p)
+	fmt.Printf("%v\n", boolValue)
 }
