fde6fac969cf480424663646afa6f8c2ba3d02aa
teng <wwwsousoucom@126.com>
2017-11-29 22:23:31 +0800

    解释

--- lesson2/flag_test/flag_try.go
+++ lesson2/flag_test/flag_try.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"strings"
 )
-
+// String类型的flag
 var sep = flag.String("s", " ", "separator")
 
 func main() {
 	flag.Parse()
+	// *sep 指向内容
 	fmt.Println(strings.Join(flag.Args(), *sep))
 }
 
