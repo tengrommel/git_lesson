79bfed3a4fb7b58496547764c0012c4e3436e2f2
teng <wwwsousoucom@126.com>
2017-11-29 21:41:25 +0800

    局部变量

--- lesson2/init_.go
+++ lesson2/init_.go
@@ -7,18 +7,20 @@ import "fmt"
 bool值的默认值为false
 初始化为零值 安全
  */
-
-func main() {
-	var (
+var (
 		x int
 		y float32
 		z string
 		p *int
 		boolValue bool
 	)
+
+func main() {
+	i:=0
 	fmt.Printf("%v\n", x)
 	fmt.Printf("%v\n", y)
 	fmt.Printf("%v\n", z)
 	fmt.Printf("%v\n", p)
 	fmt.Printf("%v\n", boolValue)
+	fmt.Printf("%v\n", i)
 }
