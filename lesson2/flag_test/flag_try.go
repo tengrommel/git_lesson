22e88587f1e6aec5bccdc8048959cd7e7cce3f3e
teng <wwwsousoucom@126.com>
2017-11-30 22:25:30 +0800

    newline

--- lesson2/flag_test/flag_try.go
+++ lesson2/flag_test/flag_try.go
@@ -7,10 +7,16 @@ import (
 )
 // String类型的flag
 var sep = flag.String("s", " ", "separator")
+var newline = flag.Bool("n", false, "append newline")
+
 
 func main() {
 	flag.Parse()
-	// *sep 指向内容
+	// *sep 指向内容 Join拼接列表
 	fmt.Println(strings.Join(flag.Args(), *sep))
+	if *newline {
+		fmt.Println()
+	}
+
 }
 
