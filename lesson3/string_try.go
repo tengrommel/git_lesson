442eff621ed1ba39a58ac5d67fc426a290d1dca7
tengrommel <wwwsousoucom@126.com>
2017-12-03 00:22:43 +0800

    字符串操作

--- lesson3/string_try.go
+++ lesson3/string_try.go
@@ -7,5 +7,16 @@ import (
 
 func main() {
 	 str1 := "hello\\world\b" // 制表符 \b
+	 str2 := ""
+	 if str2 == str1{
+
+	 }
+	 // byte
+	 c1 := str1[1]
+	 fmt.Println(0, len(str1)-1)
 	 fmt.Println(str1)
+	 fmt.Println(c1)
+
+	 s3 := str1[3:6]
+	 fmt.Println(s3)
 }
