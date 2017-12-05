ba9f063c07cccf3f04c9b780e063b1b69b79c503
tengrommel <wwwsousoucom@126.com>
2017-12-05 22:56:48 +0800

    error

---
+++ lesson5/error.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+	"io/ioutil"
+	"log"
+	"time"
+	"fmt"
+)
+
+func read(f *os.File) (string, error) {
+	buf, err := ioutil.ReadAll(f)
+	if err != nil{
+		return "", err
+	}
+	return string(buf), nil
+}
+
+func main() {
+	f, err := os.Open("a.txt")
+	if err != nil{
+		log.Fatal(err)
+	}
+	var content string
+	retries := 3
+	for i:=1;i<=retries;i++ {
+		content, err = read(f)
+		if err == nil{
+			break
+		}
+		time.Sleep(time.Second)
+	}
+	fmt.Println(content)
+}
