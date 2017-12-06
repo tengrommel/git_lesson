44c65ea35a7d24adfb09745ff6141bae155951bc
tengrommel <wwwsousoucom@126.com>
2017-12-06 22:12:59 +0800

    classmethod

--- lesson6/path.go
+++ lesson6/path.go
@@ -9,20 +9,21 @@ type PointOne struct {
 	X, Y float64
 }
 
-func (p *PointOne)Distance(q PointOne) float64 {
+func (p PointOne)Distance(q PointOne) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
 
-func Distance(path []PointOne) float64 {
-	source := 0.0
-	for i:=0;i<len(path)-1;i++  {
-		ok:=path[i].Distance(path[i+1])
-		source = source +ok
-	}
-	return source
+type Path []PointOne
+
+func (path Path)Distance() float64 {
+	return 0
+}
+
+func (path Path)LenPoints() int {
+	return len(path)
 }
 
 func main() {
-	path := []PointOne{{1,2}, {3, 4}, {5,6}}
-	fmt.Println(Distance(path))
+	path := Path{{1,2}, {2, 3}, {3, 4}}
+	fmt.Println(path.Distance())
 }
