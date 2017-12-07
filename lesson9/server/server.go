e2c9fce5a893eb9bb7435b13b4c924f9d89eab13
tengrommel <wwwsousoucom@126.com>
2017-12-07 20:51:06 +0800

    ok

--- lesson9/server/server.go
+++ lesson9/server/server.go
@@ -5,23 +5,38 @@ import (
 	"log"
 	"os"
 	"io/ioutil"
+	"bufio"
+	"strings"
 )
 
 func handleConn(conn net.Conn)  {
 	defer conn.Close()
-	f, err := os.Open("index.html")
-	if err != nil{
-		log.Fatal(err)
+	r := bufio.NewReader(conn)
+	line, _ := r.ReadString('\n')
+	line = strings.TrimSpace(line)
+	fields := strings.Fields(line)
+	if len(fields)!=2{
+		conn.Write([]byte("bad input"))
 		return
 	}
-	defer f.Close()
-	buf, err := ioutil.ReadAll(f)
-	if err != nil{
-		log.Print(err)
-		return
+	cmd := fields[0]
+	name := fields[1]
+	if cmd == "GET" {
+		f, err := os.Open(name)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		defer f.Close()
+		buf, err := ioutil.ReadAll(f)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		conn.Write(buf)
+	}else if cmd == "STORE"{
+		// 从r读取文件内容
 	}
-	conn.Write(buf)
-	conn.Close()
 }
 
 func main() {
