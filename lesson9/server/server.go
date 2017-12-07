974180bbb788286dd0773bc5a29aaf1ba9337399
tengrommel <wwwsousoucom@126.com>
2017-12-07 20:01:15 +0800

    server

--- lesson9/server/server.go
+++ lesson9/server/server.go
@@ -12,10 +12,13 @@ func main() {
 		log.Fatal(err)
 	}
 	defer listener.Close()
-	conn, err := listener.Accept()
-	if err!= nil{
-		log.Fatal(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		conn.Write([]byte("hello golang!\n"))
+		conn.Close()
 	}
-	conn.Write([]byte("hello golang!\n"))
-	conn.Close()
 }
