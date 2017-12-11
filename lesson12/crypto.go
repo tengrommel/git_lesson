e288cd181b65f804886ed323aa229575a629eca4
tengrommel <wwwsousoucom@126.com>
2017-12-11 22:19:29 +0800

    crypto

--- lesson12/crypto.go
+++ lesson12/crypto.go
@@ -3,18 +3,22 @@ package main
 import (
 	"io"
 	"net"
+	"crypto/rc4"
 )
 
 type CryptoWriter struct {
 	w io.Writer
+	cipher *rc4.Cipher
 }
 
-func NewCryptoWriter(w io.Writer) io.Writer {
+func NewCryptoWriter(w io.Writer, key string) io.Writer {
 	return &CryptoWriter{
 		w: w,
 	}
 }
 
+// 把b里面的数据进行加密，之后写入到w.w里面
+// 调用w.w.Write进行写入
 func (w *CryptoWriter)Write(b []byte) (int, error) {
 	return 1, nil
 }
@@ -23,7 +27,7 @@ type CryptoReader struct {
 	r io.Reader
 }
 
-func NewCryptoReader(r io.Reader) io.Reader {
+func NewCryptoReader(r io.Reader, key string) io.Reader {
 	return &CryptoReader{
 		r: r,
 	}
@@ -35,9 +39,9 @@ func (r *CryptoReader)Read(b []byte) (int, error) {
 
 func main() {
 	remote, err := net.Dial("tcp", "")
-	w := NewCryptoWriter(remote)
+	w := NewCryptoWriter(remote,"123456")
 	w.Write([]byte("hello"))
-	r := NewCryptoReader(remote)
+	r := NewCryptoReader(remote, "123456")
 	buf := make([]byte, 1024)
 	r.Read(buf)
 	if err != nil{
